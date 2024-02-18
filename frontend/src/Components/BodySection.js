3fa8fd5256a53835d25e4eaa2cfae235e365f8bf
e95848cb <conor.bourne@student.manchester.ac.uk>
2024-02-18 21:34:44 +0000

    Revert "Fixed duplicate components file"
    This reverts commit 32c1178d32569478aca2a0ef01e643c2ccd79d04.

---
+++ frontend/src/Components/BodySection.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { checkForToxicity } from '../functions.js';
+
+const BodySection = () => {
+  return (
+    <div className='body-section'>
+      <h1>EDURATE</h1>
+      <p>DETAILED REVIEWS AND DISCUSSIONS OF LECTURERS AND COURSE UNITS</p>
+      {/* Add other content for your body section as needed */}
+      <form id="myForm" onSubmit={(event) => {event.preventDefault(); checkForToxicity("Hello there sir");}}>
+          <label htmlFor="fname">Text</label><br />
+          <input type="text" id="fname" name="fname" /><br />
+          <input type="submit" value="Submit" />
+        </form>   {/*Remove after testing toxicity function*/}
+    </div>
+  );
+};
+
+export default BodySection;
