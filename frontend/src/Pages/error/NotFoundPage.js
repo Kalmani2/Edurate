c21791021a32ae1199a313526ec2fdacfd5c635d
Muhammad Kalmani <muhammadkalmani@Muhammads-MacBook-Pro-3.local>
2024-03-22 01:21:48 +0300

    Cleaned up CSS for 404 error page

--- frontend/src/Pages/error/NotFoundPage.js
+++ frontend/src/Pages/error/NotFoundPage.js
@@ -1,10 +1,23 @@
 import { Link } from "react-router-dom";
+import react_logo from '../../components/react_logo.png';
 import './notFoundStyle.css';
 
-export default function NotFoundPage(){
-    return <div className="not-found-container">
-        <h1>404</h1>
-        <h1>Sorry, page not found</h1>
-        <Link to="/">Home</Link>
-    </div>;
-};
+const NotFoundPage = () => {
+    return (
+        <div>
+            <div className="not-found-container">
+                <div className="error404">404</div>
+                <h1>PAGE NOT FOUND</h1>
+                <div className="return-container">
+                    <Link to="/" className="return-button">GO HOME</Link>
+                </div>
+            </div>
+            <div className="logo-container">
+                <img src={react_logo} alt="Logo" className="error-logo"/>
+            </div>
+        </div>
+
+    );
+}
+
+export default NotFoundPage;
