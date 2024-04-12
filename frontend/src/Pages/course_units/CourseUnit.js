3a011ffd6ae99ecf13170cb85c52f81a975370d1
w13278lm <leo.makhnovskiy@student.manchester.ac.uk>
2024-04-12 01:14:00 +0100

    Course unit and lecturer page updated

--- frontend/src/Pages/course_units/CourseUnit.js
+++ frontend/src/Pages/course_units/CourseUnit.js
@@ -10,8 +10,34 @@ const CourseUnit = () => {
        <div className='home-container'>
         <h1>COURSE UNITS</h1>
        </div>
+
+       <div class="courses-viewing-options-container">
+        <div class="drop-down-container">
+          <label class="course-unit-label" for="dropdown"><b>SORT BY:</b></label>
+            <select class="course-unit-select" id="dropdown" name="sorting">
+            <option value="rating">RATING</option>
+            <option value="ascending">A TO Z</option>
+            <option value="descending">Z TO A</option>
+            </select>
+        </div>
+        <input type="text" placeholder="Search courses..."></input>
+       </div>
+
+       <div class="course-container">
+        <div class="course-unit">
+            <h3>COMP16321</h3>
+            <p>Introduction to Computer Science</p>
+            <button class="info-button" onclick="showInfo('COMP16321')">More Info</button>
+        </div>
+        <div class="course-unit">
+            <h3>COMP10120</h3>
+            <p>Foundations of Computation</p>
+            <button class="info-button" onclick="showInfo('COMP10120')">More Info</button>
+        </div>
+       </div>
+    
     </div>
   )
 }
 
-export default CourseUnit
+export default CourseUnit;
--- frontend/src/Pages/lecturers/Lecturers.js
+++ frontend/src/Pages/lecturers/Lecturers.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
 import './lecturersStyle.css';
+import { Link } from "react-router-dom";
 
 const Lecturers = () => {
   return (
@@ -9,8 +10,36 @@ const Lecturers = () => {
        <div className='home-container'>
         <h1>LECTURERS</h1>
        </div>
+
+       <div class="lecturer-viewing-options-container">
+        <div class="drop-down-container">
+          <label class="lecturer-label" for="dropdown"><b>SORT BY:</b></label>
+            <select class="lecturer-select" id="dropdown" name="sorting">
+            <option value="rating">RATING</option>
+            <option value="ascending">A TO Z</option>
+            <option value="descending">Z TO A</option>
+            </select>
+        </div>
+        <input type="text" placeholder="Search lecturers..."></input>
+       </div>
+
+       <div class="lecturer-container">
+        <div class="lecturer">
+            <h3>Somebody1</h3>
+            <p>Some description</p>
+            <button class="info-button" onclick="showInfo('Somebody1')">More Info</button>
+        </div>
+        <div class="lecturer">
+            <h3>Somebody2</h3>
+            <p>Some description</p>
+            <button class="info-button" onclick="showInfo('Somebody2')">More Info</button>
+        </div>
+       </div>
+
     </div>
+
+    
   )
 }
 
-export default Lecturers
+export default Lecturers;
